fbb0c66104f777729fe5a6ec9e1e979ef8766fab
Venkatesh karamala <venkatesh.karamala@gmail.com>
2019-01-10 14:51:48 -0500

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 addNewServer=false;
+newserverstatus="No Server Created";
+textentered='';
   constructor() {
     setTimeout(() => {this.addNewServer=true;},2000)
    }
 
   ngOnInit() {
   }
-
+onServerCreate() 
+{
+this.newserverstatus="New Server Created";
+}
+enterservername(event: Event)
+{
+  console.log(event);
+ this.textentered=(<HTMLInputElement>event.target).value;
+}
 }
